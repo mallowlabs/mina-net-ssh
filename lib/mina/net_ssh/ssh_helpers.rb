368e8667178ff86b026fae209823db65aec9732b
mallowlabs <mallowlabs@gmail.com>
2013-05-02 23:23:24 +0900

    Added: using net-ssh

--- lib/mina/net_ssh/ssh_helpers.rb
+++ lib/mina/net_ssh/ssh_helpers.rb
@@ -3,7 +3,22 @@ require "net/ssh"
 module Mina
   module SshHelpers
     def ssh(cmd, options={})
-      puts cmd
+      cmd = cmd.join("\n")  if cmd.is_a?(Array)
+
+      if simulate_mode?
+        str = "Executing the following via '#{ssh_command}':"
+        puts "#!/usr/bin/env bash"
+        puts "# #{str}"
+        puts "#"
+
+        puts cmd
+      else
+        Net::SSH.start(domain!, user, :password => password) do |s|
+          cmd.split("\n").each do |line|
+            puts s.exec!(line)
+          end
+        end
+      end
     end
   end
 end
